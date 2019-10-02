058a56e2e8ee28f8107b345947eaa22442605d59
Ben <benjisgarza@gmail.com>
2019-10-02 15:57:22 -0700

    Started on creating an "Edit users" page

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User[:id]
+  end
+
   def create
     @user = User.new(user_params)
 
--- re-former/config/routes.rb
+++ re-former/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post '/new',  to: 'users#create'
+  get '/users/edit',  to: 'users#edit'
+  post '/users/new',  to: 'users#create'
   resources :users, only: [:new, :create]
 end
