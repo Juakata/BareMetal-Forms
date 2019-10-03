e602007973e6dda99de0ad10b3ca18572e8be429
Juakata <ha.uzquiano@hotmail.com>
2019-10-03 09:57:58 -0600

    working on edit

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
+
+
   def new
     @user = User.new
   end
 
   def edit
-    @user = User[:id]
+    @user = User.find(params[:id])
   end
 
   def create
@@ -16,6 +18,14 @@ class UsersController < ApplicationController
       render :new
     end
 
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
   end
   private
 
--- re-former/config/routes.rb
+++ re-former/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/users/edit',  to: 'users#edit'
+  get '/users/:id/edit',  to: 'users#edit'
   post '/users/new',  to: 'users#create'
   resources :users, only: [:new, :create]
 end
