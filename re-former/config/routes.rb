f65e013ccd73a9735cdbf93ecf04827fb161aa9f
Ben <benjisgarza@gmail.com>
2019-10-03 12:28:51 -0700

    Linter fix: double quotes to single, spacing fixes

--- re-former/config/routes.rb
+++ re-former/config/routes.rb
@@ -2,9 +2,9 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #get '/users/:id/edit',  to: 'users#edit'
-  get '/users/:id/edit',  to: 'users#edit'
-  #post 'users/:id/update', to 'users#update'
-  post '/users/new',  to: 'users#create'
+  # get '/users/:id/edit', to: 'users#edit'
+  get '/users/:id/edit', to: 'users#edit'
+  # post 'users/:id/update', to 'users#update'
+  post '/users/new', to: 'users#create'
   resources :users
 end
