ffc61c2f01ada6da2dffec5547c60f7f03a4419e
Ben <benjisgarza@gmail.com>
2019-10-03 09:48:39 -0700

    Finish edit page, link user show to edit, display error messages

--- re-former/config/routes.rb
+++ re-former/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/users/:id/edit',  to: 'users#edit'
+  #post 'users/:id/update', to 'users#update'
   post '/users/new',  to: 'users#create'
-  resources :users, only: [:new, :create]
+  resources :users
 end
